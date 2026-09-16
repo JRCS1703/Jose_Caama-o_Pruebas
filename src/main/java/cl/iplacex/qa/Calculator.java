205b1a91cfbd25abc46877564fd3de16cd1df84f
José Caamaño <jrcs1703@gmail.com>
2026-09-16 16:44:50 -0300

    feat: agrega servicio Calculator

---
+++ src/main/java/cl/iplacex/qa/Calculator.java
@@ -0,0 +1,15 @@
+package cl.iplacex.qa;
+
+/**
+ * Servicio simple utilizado para demostrar pruebas unitarias automatizadas.
+ */
+public final class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
